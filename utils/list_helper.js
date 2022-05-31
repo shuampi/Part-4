c4e8535f49b40c5071e7aa9f05a77e70bbcbece3
shuamp <castel.davila@gmail.com>
2022-05-31 15:38:03 +0100

    first test total likes passed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,10 +3,20 @@ const dummy = (blogs) => {
     return 1
  }
 
+ const totalLikes = (arrayBlogs) => { 
+     if(arrayBlogs.length!==0){
+    const likesArray = arrayBlogs.map((blogList) => { blogList.likes })
+    const sum = likesArray.reduce((previus, current) => { previus + current })}
+
+    return arrayBlogs.length===0? 0: sum
+
+  }
+
 
 
 
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
