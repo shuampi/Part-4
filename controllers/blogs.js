959ba7ff174a256e98b3f090a11d47add5276442
shuamp <castel.davila@gmail.com>
2022-06-08 15:42:09 +0100

    exercise 4.8 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,12 +3,14 @@
 const router = require('express').Router()
 const Blog = require('../models/blog')
 
-router.get('/', (request, response) => {
-    Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+router.get('/',async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+    // Blog
+    // .find({})
+    // .then(blogs => {
+    //   response.json(blogs)
+    // })
 })
 
 router.post('/', (request, response) => {
