8a85eafcf75774f107d13749c6c37a835ad14388
shuamp <castel.davila@gmail.com>
2022-06-08 12:33:46 +0100

    initialize database every time the tests are run

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,48 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const Blog = require('../models/blog')
 const app = require("../app")
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+ "title": "data 1 test blog",
+  "author": "Pepito",
+  "url": "http://lawebpepito.com",
+  "likes": 9
+},
+{
+  "title": "data 2 test blog",
+   "author": "Menganito",
+   "url": "http://lawebMenganito.com",
+   "likes": 41
+ },
+ {
+  "title": "data 3 test blog",
+   "author": "Fulanito",
+   "url": "http://lawebFulanito.com",
+   "likes": 41
+ },
+ {
+  "title": "data 4 test blog",
+   "author": "Chiquito",
+   "url": "http://lawebChiquito.com",
+   "likes": 16
+ }
+]
+
+beforeEach(async () => { 
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[3])
+  await blogObject.save()
+ })
 
 test('blogs are return as json', async() =>
 {
@@ -11,6 +51,18 @@ await api
 .expect(200)
 .expect('Content-Type', /application\/json/)
 })
+
+test('all the blogs are return', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a especific blogger author is within returned blog list', async () => {
+const response = await api.get('/api/blogs')
+const authors = response.body.map(blog => blog.author)
+expect(authors).toContain('Pepito')
+ })
 
 afterAll(() => {
     mongoose.connection.close()
--- utils/logger.js
+++ utils/logger.js
@@ -1,13 +1,18 @@
 //module for printing to the console.
 
 const info = (...params) => {
-    console.log(...params)
-}
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
+  }
+};
 
 const error = (...params) => {
-    console.error(...params)
-}
+  if (process.env.NODE_ENV !== "test") {
+    console.error(...params);
+  }
+};
 
 module.exports = {
-    info, error
-}
+  info,
+  error,
+};
